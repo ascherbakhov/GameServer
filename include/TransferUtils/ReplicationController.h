80c429ce04f57b6399ae80a8ff723d13047a7bc3
ascherbakhov <a.scherbakhov@gmail.com>
2019-05-01 01:40:55 +0300

    Small refactoring

---
+++ include/TransferUtils/ReplicationController.h
@@ -0,0 +1,14 @@
+//
+// Created by qazzer on 01.05.2019.
+//
+
+#ifndef NETWORK_REPLICATIONCONTROLLER_H
+#define NETWORK_REPLICATIONCONTROLLER_H
+
+
+class ReplicationController {
+
+};
+
+
+#endif //NETWORK_REPLICATIONCONTROLLER_H
---
+++ src/TransferUtils/ReplicationController.cpp
@@ -0,0 +1,5 @@
+//
+// Created by qazzer on 01.05.2019.
+//
+
+#include "TransferUtils/ReplicationController.h"
